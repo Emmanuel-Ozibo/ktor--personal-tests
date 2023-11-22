8b671e17cbc20bb6dec7d8b17a99a273ebc60c10
EmmanuelOzibo <eozibo@league.com>
2023-11-22 14:52:58 +0100

    removed unused imports

--- src/main/kotlin/com/example/routes/user/UserRoutes.kt
+++ src/main/kotlin/com/example/routes/user/UserRoutes.kt
@@ -1,13 +1,7 @@
 package com.example.routes.user
 
-import com.example.extensions.doesUserExists
-import com.example.extensions.isNotNull
-import com.example.extensions.orEmpty
+
 import com.example.models.User
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.userRouting() {
